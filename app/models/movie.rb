c85d82c401f9f662e1d677bf52802a95f2e9bbf4
Sheena Sang <sheenasany@gmail.com>
2022-09-05 21:27:13 -0400

    commit tree 1ac45827f241b48252dacf06ef3ba959e66b4958

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,50 @@
 class Movie < ActiveRecord::Base
+    def self.create_with_title(title)
+        Movie.create(title: title)
+    end
+
+    def self.first_movie
+        Movie.first
+    end
+
+    def self.last_movie
+        Movie.last
+    end
+
+    def self.movie_count
+        Movie.count
+    end
+
+    def self.find_movie_with_id(id)
+        Movie.find(id)
+    end
+
+    def self.find_movie_with_attributes(attribute)
+        Movie.find_by(attribute)
+    end
+
+    def self.find_movies_after_2002
+        Movie.where("release_date > 2002")
+        # Movie.where("release_date > ?", 2002)
+        #quotes stand for interpolation within the where
+        # adding the ? acts as a placeholder, lessens the risk of injection errors
+        # interpolates the condition, include a comma to separate the logic against interpolation
+    end
+
+    def update_with_attributes(attribute)
+        self.update(attribute)
+    end
+
+    def self.update_all_titles(title)
+        Movie.update(title: title)
+    end
+
+    def self.delete_by_id(id)
+        Movie.destroy(id)
+    end
+
+    def self.delete_all_movies
+        Movie.destroy_all
+    end
 
 end
